b01e4ae309737fd257377220b7068815a72f8a1a
teppei222 <75667712+teppei222@users.noreply.github.com>
2021-04-10 11:14:13 +0900

    Update find_map.py

--- find_map.py
+++ find_map.py
@@ -5,6 +5,20 @@ import streamlit.components.v1 as components
 import numpy as np
 
 
+# Everything is accessible via the st.secrets dict:
+
+st.write("DB username:", st.secrets["db_username"])
+st.write("DB password:", st.secrets["db_password"])
+
+# And the root-level secrets are also accessible as environment variables:
+
+import os
+st.write(
+	"Has environment variables been set:",
+	os.environ["db_username"] == st.secrets["db_username"])
+
+
+
 
 
 df_header = pd.read_csv('./DB2.csv',encoding="cp932")
