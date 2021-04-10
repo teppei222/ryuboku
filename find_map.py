5f9282064607252cf4b4d19d4f9bc7e9b01342f4
teppei222 <75667712+teppei222@users.noreply.github.com>
2021-04-10 11:15:45 +0900

    Update find_map.py

--- find_map.py
+++ find_map.py
@@ -37,20 +37,6 @@ st.header('該当施設検索')
 st.table(df)
 
 
-# Everything is accessible via the st.secrets dict:
-
-st.write("DB username:", st.secrets["db_username"])
-st.write("DB password:", st.secrets["db_password"])
-
-# And the root-level secrets are also accessible as environment variables:
-
-import os
-st.write(
-	"Has environment variables been set:",
-	os.environ["db_username"] == st.secrets["db_username"])
-
-
-
 midpoint = (np.average(df['lon']), np.average(df['lat']))
 
 tooltip = {
