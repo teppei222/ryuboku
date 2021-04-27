35cc6869241a8fd00c1433e3a1c20a88e048577f
teppei222 <75667712+teppei222@users.noreply.github.com>
2021-04-27 17:03:42 +0900

    Update main.py

--- main.py
+++ main.py
@@ -25,8 +25,8 @@ if uploaded_file is not None:
     minDist=42
     param1=100
     param2=15
-    minRadius=21
-    maxRadius=29
+    minRadius=18
+    maxRadius=26
     gazou_waku_xstart=590
     gazou_waku_xend=2390
     gazou_waku_ystart=1070
