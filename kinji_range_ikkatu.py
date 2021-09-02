ad8a3adf7c1b2d759c16ce6518ded0c8a0e2b487
teppei222 <75667712+teppei222@users.noreply.github.com>
2021-09-02 16:56:24 +0900

    Update kinji_range_ikkatu.py

--- kinji_range_ikkatu.py
+++ kinji_range_ikkatu.py
@@ -4,7 +4,6 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import plotly.express as px
-from streamlit_plotly_events import plotly_events
 from cross_point import cross_point
 import io
 import base64
