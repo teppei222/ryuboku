d9d374b7620c0346ec618711cd40471759a48512
zerozeroT <75667712+teppei222@users.noreply.github.com>
2022-05-02 10:56:39 +0900

    Update camera.py

--- camera.py
+++ camera.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 st.write("表題やテキスト")
-picture = st.camera_input("Take a picture")
+picture = st.camera_input("Take a picture",help=True)
 
 if picture:
      st.image(picture)
