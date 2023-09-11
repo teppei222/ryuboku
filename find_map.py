b992e52c3ed93404b12f74d722df5dc5c58fb02c
zerozeroT <75667712+teppei222@users.noreply.github.com>
2023-09-11 19:47:17 +0900

    Update find_map.py

--- find_map.py
+++ find_map.py
@@ -28,7 +28,7 @@ df = df[df['型式'].isin(selected_options_basho)]
 
 
 name = df["堰堤名"].unique().tolist()
-container_c = st.sidebar.beta_container()
+container_c = st.sidebar.container()
 selected_options_name =  container_c.multiselect("堰堤を選んでください:",name)
 df = df[df['堰堤名'].isin(selected_options_name)]
 
